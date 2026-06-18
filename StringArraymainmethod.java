a22049fd0a2fafde35d056c745ef980334192cbf
AkshayBhange <akshaybhange720@gmail.com>
2026-06-18 23:36:34 +0530

    Pushing String Array program

---
+++ StringArraymainmethod.java
@@ -0,0 +1,14 @@
+package Module3;
+
+public class StringArraymainmethod
+{
+  public static void main(String[] args) 
+  {
+	  String a = "Auto";
+	  assert a.isEmpty();
+	  
+	  System.out.println("execute the programm");
+	
+}
+
+}
---
+++ StringArraymainmethod2.java
@@ -0,0 +1,12 @@
+package Module3;
+
+public class StringArraymainmethod2 
+{
+	public static void main(String[] args) 
+	{
+		System.out.println("Username->" + args[0]);
+		System.out.println("Username->" + args[1]);
+		
+	}
+
+}
